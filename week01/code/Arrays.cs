f91bc1a31252fea08526384a7f2de9ff39568c87
eddiedhin <sekirangi1@gmail.com>
2025-07-10 13:27:02 +0300

    gitts

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //First we will create a list to store our multiples
+        double[] multiples = new double[length];
+
+        //Now we want to populate the list with the multiples of a given number
+
+        for (int i = 0; i < length; i++)
+        {
+            //We use (i + 1) to prevent us from starting at 0
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -23,11 +34,31 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
+    
+
+    //I modified the function to be able to return the result of the list
+    public static List<int> RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //First we need to keep track of the count and store it in this variable
+        int count = data.Count;
+
+        //Create a list that is editable, A Dynamic list and declare it to not exceed the data limits
+        List<int> rotated = new List<int>(new int[count]);
+
+        //We will now populate the list using modulo operator to do the rotation
+        for (int i = 0; i < count; i++)
+        {
+            //Ensures that if the index exceeds the list size, it wraps around to the beginning
+            int newIndex = (i + amount) % count;
+            
+            rotated[newIndex] = data[i]; //Move element to new position
+        }
+
+        return rotated;
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,31 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 20, 40, 60, 80, 100 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
-        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
+        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}");
+        // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        //first we need to create a list to append array 1 and array 2
+        int[] result = new int[select.Length];
+
+        //and now we need to keep track of the indices in each of the 2 lists
+        int index1 = 0;
+        int index2 = 0;
+
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+
+                result[i] = list1[index1++];
+            else
+                result[i] = list2[index2++];
+
+        }
+        return result;
     }
 }
